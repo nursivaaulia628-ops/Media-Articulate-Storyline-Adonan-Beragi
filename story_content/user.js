8f96d9257fd44f235ef0ca7836e7a1e862b7ad8d
nursivaaulia628-ops <nursivaaulia628@gmail.com>
2026-05-03 10:12:35 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lrfb946rNU":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src='1.mp3";
+audio.load():
+audio.play();
+audio.volume=0.4;
+}
+
